4f3dcc1f87a0c3f64e47da5f6d108ca610587885
Siphiwe <stapisi2061@protonmail.com>
2022-09-09 10:44:00 +0200

    recreating db to allow for more than 50 chars.

--- main.py
+++ main.py
@@ -35,11 +35,11 @@ class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
     name = db.Column(db.String(100), nullable=False)
 
 
-# db.create_all()
+db.create_all()
 
 # To manually register users for now.
 def register_user(name, email, password):
